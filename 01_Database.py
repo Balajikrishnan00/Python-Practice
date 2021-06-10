f80521c249f8873e1f182163ff05512667ef8f65
balaji <balajikrishnan00@gmail.com>
2021-06-10 18:34:14 +0300

    updated files

--- 01_Database.py
+++ 01_Database.py
@@ -5,7 +5,7 @@ import mysql.connector
 mydatase=mysql.connector.connect(host='localhost',user='root',password='root')
 
 print(mydatase)
-"""
+
 import mysql.connector
 con=mysql.connector.connect(host='localhost',user='root',password='root')
 newDatabase='CREATE DATABASE HELLODB;'
@@ -13,4 +13,24 @@ newDatabase='CREATE DATABASE HELLODB;'
 cusr=con.cursor()
 cusr.execute(newDatabase)
 print('Database created successfully')
+"""
+import mysql.connector
+def NewDatabase(dbname):
+    connection=mysql.connector.connect(host='localhost',user='root',password='root')
+    cusr=connection.cursor()
+    cusr.execute(f'CREATE DATABASE {dbname};')
+    connection.commit()
+    print('database create successfully.')
 
+def DeleteDatabase(dbname):
+    connection = mysql.connector.connect(host='localhost', user='root', password='root')
+    crsr=connection.cursor()
+    crsr.execute(f'DROP DATABASE {dbname};')
+    connection.commit()
+    print('database delete successfully.')
+
+
+
+dbname=input('Enter DB name:')
+#NewDatabase(dbname)
+DeleteDatabase(dbname)
